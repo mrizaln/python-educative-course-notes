f4fc391d391d5275b379a32f1ab73c7701543f3c
mrizaln <mrizaln2000@gmail.com>
2022-01-27 16:04:43 +0700

    update some file

--- Data Structures and Algorithm/2. singly linked list/singly_linked_list.py
+++ Data Structures and Algorithm/2. singly linked list/singly_linked_list.py
@@ -284,10 +284,59 @@ class LinkedList:
             p.next = None
 
     # determine if a linked list is a palindrome
-    def is_palindrome(self):
-        pass
+    def is_palindrome_1(self):
+        # Solution 1: using a string
+        s = ""
+        p = self.head
+        while p:
+            s += p.data
+            p = p.next
+        return s == s[::-1]
 
+    def is_palindrome_2(self):
+        # Solution 2: using a stack
+        s = []
+        p = self.head
 
+        while p:
+            s.append(p.data)
+            p = p.next
+        p = self.head
+        while p:
+            data = s.pop()
+            if p.data != data:
+                return False
+            p = p.next
+        return True
+
+    def is_palindrome_3(self):
+        # Solution 3: using two pointer
+        if self.head:
+            p = self.head
+            q = self.head
+            prev = []
+
+            i = 0
+            while q:
+                prev.append(q)
+                q = q.next
+                i += 1
+            q = prev[i-1]
+
+            count = 1
+            while count <= i//2 + 1:
+                if prev[-count].data != p.data:
+                    return False
+                p = p.next
+                count += 1
+            return True
+
+        else: return True
+
+    def is_palindrome(self, method):
+        if method == 1: return self.is_palindrome_1()
+        elif method == 2: return self.is_palindrome_2()
+        elif method == 3: return self.is_palindrome_3()
 
 
 
@@ -413,3 +462,13 @@ if __name__ == "__main__":
     print("rotate by pivot: 4th node")
     llist_2.rotate(4)
     llist_2.print_list()
+
+    print("\n\nis palindrome?")
+    llist = LinkedList()
+    for c in "heheh": llist.append(c)
+    llist.print_list()
+    print(llist.is_palindrome(1), "\n")
+
+    llist.append(c)
+    llist.print_list()
+    print(llist.is_palindrome(2))
